bcaf7558d90bb156b7a7da344586c8b7c147c0cf
Aitor <aitortxu92m@gmail.com>
2016-02-27 13:31:15 +0100

    Added arguments

---
+++ src/main/java/Arguments.java
@@ -0,0 +1,78 @@
+import com.github.jankroken.commandline.annotations.*;
+
+/**
+ * Created by Aitor on 27/2/16.
+ */
+public class Arguments {
+
+    private String confFilename;
+    private String port;
+    private String mdir;
+    private String mport;
+    private boolean debug = false;
+
+    @Option
+    @LongSwitch("conffile")
+    @ShortSwitch("f")
+    @SingleArgument
+    @Required
+    public void setConfFilename(String filename) {
+        this.confFilename = filename;
+    }
+
+    @Option
+    @LongSwitch("port")
+    @ShortSwitch("p")
+    @SingleArgument
+    @Required
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    @Option
+    @LongSwitch("mdir")
+    @ShortSwitch("m")
+    @SingleArgument
+    @Required
+    public void setMdir(String mdir) {
+        this.mdir = mdir;
+    }
+
+    @Option
+    @LongSwitch("mport")
+    @ShortSwitch("o")
+    @SingleArgument
+    @Required
+    public void setMport(String mport) {
+        this.mport = mport;
+    }
+
+    @Option
+    @LongSwitch("debug")
+    @ShortSwitch("d")
+    @Toggle(true)
+    public void setDebug(boolean debug) {
+        this.debug = debug;
+    }
+
+    public String getConfFilename() {
+        return confFilename;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getMdir() {
+        return mdir;
+    }
+
+    public String getMport() {
+        return mport;
+    }
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+}
---
+++ src/main/java/s_server.java
@@ -0,0 +1,48 @@
+import com.github.jankroken.commandline.CommandLineParser;
+import com.github.jankroken.commandline.OptionStyle;
+import com.github.jankroken.commandline.domain.InvalidCommandLineException;
+import com.github.jankroken.commandline.domain.InvalidOptionConfigurationException;
+import com.github.jankroken.commandline.domain.UnrecognizedSwitchException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by Aitor on 27/2/16.
+ */
+public class s_server {
+
+    private static final Logger LOGGER = Logger.getLogger( s_server.class.getName() );
+
+    public static void main(String[] args) {
+
+        LOGGER.setLevel(Level.ALL);
+
+        try {
+            Arguments arguments = CommandLineParser.parse(Arguments.class, args, OptionStyle.SIMPLE);
+
+            LOGGER.info("Arguments: -f " + arguments.getConfFilename() + " -p " + arguments.getPort() + " -m "
+                + arguments.getMdir() + " -o " + arguments.getMport());
+
+
+            LOGGER.info("Starting server... ");
+
+
+        } catch (InvalidCommandLineException clException) {
+            clException.printStackTrace();
+        } catch (InvalidOptionConfigurationException configException) {
+            configException.printStackTrace();
+        } catch (UnrecognizedSwitchException unrecognizedSwitchException) {
+            unrecognizedSwitchException.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
